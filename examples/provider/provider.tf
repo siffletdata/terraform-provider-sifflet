80fcbe58ce8efd8d176b2f9ed643773ccab647b5
Benoît Faucon <benoit@siffletdata.com>
2024-09-24 17:46:14 +0200

    docs: improve provider configuration example with plausible hostname

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,4 +1,7 @@
 provider "sifflet" {
-  host  = "http://localhost:8080"
+  host = "https://tenant.siffletdata.com/api"
+  // Sifflet API token. If not set, the token will be read from the SIFFLET_TOKEN environment variable.
+  // (it's recommended to use environment variables to avoid storing sensitive information in your
+  // configuration)
   token = "123azert"
 }
